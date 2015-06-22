92f9e239a6ad00bc867979798909c0096469ac48
Lijing Wang <ooofreedom@gmail.com>
2015-06-22 09:12:16 -0700

    change consume way add thread pool

--- src/test_speed_consumer.cpp
+++ src/test_speed_consumer.cpp
@@ -9,17 +9,116 @@
 #include <boost/thread/thread.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/ini_parser.hpp>
+#include <thread>
+#include <deque>
 
 // Enclosing code in ndn simplifies coding (can also use `using namespace ndn`)
 namespace ndn {
 // Additional nested namespace could be used to prevent/limit name contentions
+
+#define VIDEO_RATE 7
+#define AUDIO_RATE 6
+
+  struct Consumer_Need
+  {
+    int end;
+    int rate;
+    std::string name;
+    ConsumerCallback *cb;
+    Consumer* sampleConsumer;
+  };
+
+  void
+  consumer_setup(Consumer_Need *con)
+  {
+    if(con->name == "video")
+    {
+      con->sampleConsumer->setContextOption(CONTENT_RETRIEVED, 
+                          (ConsumerContentCallback)bind(&ConsumerCallback::processTestPayload, con->cb, _1, _2, _3));
+
+    }else
+    {
+      con->sampleConsumer->setContextOption(CONTENT_RETRIEVED, 
+                          (ConsumerContentCallback)bind(&ConsumerCallback::processTestPayloadAudio, con->cb, _1, _2, _3));
+    }
+        
+    con->sampleConsumer->setContextOption(MUST_BE_FRESH_S, true);
+    con->sampleConsumer->setContextOption(INTEREST_LIFETIME, 500);
+
+    con->sampleConsumer->setContextOption(INTEREST_RETX,16); //Retransmitted Attempted Time.
+
+    con->sampleConsumer->setContextOption(INTEREST_LEAVE_CNTX, 
+                              (ConsumerInterestCallback)bind(&ConsumerCallback::processLeavingInterest, con->cb, _1, _2));
+    con->sampleConsumer->setContextOption(INTEREST_RETRANSMIT, 
+                              (ConsumerInterestCallback)bind(&ConsumerCallback::onRetx, con->cb, _1, _2));
+    con->sampleConsumer->setContextOption(INTEREST_EXPIRED, 
+                              (ConsumerInterestCallback)bind(&ConsumerCallback::onExpr, con->cb, _1, _2));
+    con->sampleConsumer->setContextOption(DATA_ENTER_CNTX, 
+                              (ConsumerDataCallback)bind(&ConsumerCallback::processData, con->cb, _1, _2));
+  }
+
+  time_t time_start;
+  ConsumerCallback cb_consumer(VIDEO_RATE, AUDIO_RATE); 
+
+  static void sig_int(int num)
+  {
+    time_t time_end = std::time(0);
+    double seconds = difftime(time_end, time_start);
+    std::cout << "  " << std::endl;
+    std::cout << "｡:.ﾟヽ(*´∀`)ﾉﾟ. Test Result ヽ(✿ﾟ▽ﾟ)ノ  " << std::endl;
+    std::cout << "  " << std::endl;
+    std::cout << "-------- Throughput --------  " << std::endl;
+    std::cout << "Video Bytes: " << std::dec << cb_consumer.payload_v << "  PayLoad_Throughput: " << cb_consumer.payload_v/seconds << " Bytes/Seconds" <<std::endl;
+    std::cout << "Audio Bytes: " << cb_consumer.payload_a << "  PayLoad_Throughput: " << cb_consumer.payload_a/seconds << " Bytes/Seconds" <<std::endl;
+    std::cout << "Total Bytes: " << cb_consumer.payload_v + cb_consumer.payload_a << "  PayLoad_Throughput: " << (cb_consumer.payload_v + cb_consumer.payload_a)/seconds << " Bytes/Seconds" << std::endl;
+
+    std::cout << "Video Frames: " << std::dec << cb_consumer.frame_cnt_v << "  Frame_Throughput: " << cb_consumer.frame_cnt_v/seconds << " /Seconds" <<std::endl;
+    std::cout << "Audio Frames: " << std::dec << cb_consumer.frame_cnt_a << "  Frame_Throughput: " << cb_consumer.frame_cnt_a/seconds << " /Seconds" <<std::endl;
+    std::cout << "Total Frames: " << cb_consumer.frame_cnt_v + cb_consumer.frame_cnt_a << "  Frame_Throughput: " << (cb_consumer.frame_cnt_v + cb_consumer.frame_cnt_a)/seconds << " /Seconds" << std::endl;
+
+    std::cout << "  " << std::endl;
+    std::cout << "-------- Interest --------  " << std::endl;
+    std::cout << "Interest_Send: " << cb_consumer.interest_s << "  Interest_Send_Speed: " <<  cb_consumer.interest_s/seconds << " /Seconds" <<  std::endl;
+    std::cout << "Interest_Receive: " << cb_consumer.interest_r << "  Interest_Receive_Speed: " <<  cb_consumer.interest_r /seconds << " /Seconds" <<  std::endl;
+    std::cout << "Interest_Retransmit: " << cb_consumer.interest_retx << " Interest_Retransfer_Speed: " << cb_consumer.interest_retx/seconds << " /Seconds" << std::endl;
+    std::cout << "Interest_Expire: " << cb_consumer.interest_expr << " Interest_Retransfer_Speed: " << cb_consumer.interest_expr/seconds << " /Seconds" << std::endl;
+    std::cout << "Interest_Useful Percentage: " << double(cb_consumer.interest_r)/double(cb_consumer.interest_s)*100 <<"%" << std::endl;
+    std::cout << "  " << std::endl;
+    std::cout << "---------- OVER ----------  " << seconds <<" seconds" << std::endl;
+
+    return;
+  }
+
+  void consume_frame(std::string type, Consumer *sampleConsumer, int frameNumber) 
+  {
+    Name sampleSuffix(std::to_string(frameNumber));
+    printf("%s Number %d before cosume\n", type.c_str(), frameNumber);
+    sampleConsumer->consume(sampleSuffix);
+    printf("%s Number %d after cosume\n", type.c_str(), frameNumber);
+  }
+
+  void consume_control(Consumer *sampleConsumer, int end)
+  {
+    signal(SIGINT, sig_int);
+    for (int i = 0; i < end; i++)
+    { 
+      Name sampleSuffix(std::to_string(i));
+
+      signal(SIGINT, sig_int);
+      printf("Number %d before cosume End:%d\n",i, end);
+      sampleConsumer->consume(sampleSuffix);
+      printf("Number %d after cosume End:%d\n",i, end);
+
+    }
+    sig_int(0);
+    pthread_exit(NULL);
+  }
 
   int
   main(int argc, char** argv)
   {
     try {
   
-
       boost::property_tree::ptree pt;
       boost::property_tree::ini_parser::read_ini("config.ini", pt);
 
@@ -63,39 +162,130 @@ namespace ndn {
       }else
         filename = argv[1];
 
-      signal(SIGINT, sig_int);
 
       std::cout << "Asking For " << filename << std::endl;
       std::cout << "Only for speed test" <<std::endl;
 
-
-      int rc_audio;
-      Consumer_Need audioData;
-      audioData.prefix = Name(prefix).append(filename);
-      audioData.name = "audio";
-      audioData.cb = &cb_consumer ;
-      pthread_t thread_audio; 
-
       time_start = std::time(0);
 
-      rc_audio = pthread_create(&thread_audio, NULL, consume_thread , (void *)&audioData);
 
-      int rc_video;
+
+      Consumer_Need audioData;
+      audioData.name = "audio";
+      audioData.end = 13838 / 10; 
+      audioData.cb = &cb_consumer;
+      audioData.rate = AUDIO_RATE; //23;
+      Name audioName = Name(prefix).append(filename).append("audio").append("content");
+//      std::thread audioThread(consume_control, audioData.sampleConsumer, audioData.end);
+      
+//      tp.schedule(boost::bind(consume_control, audioData.sampleConsumer, audioData.end));
+
       Consumer_Need videoData;
-      videoData.prefix = Name(prefix).append(filename);
       videoData.name = "video";
-      videoData.cb = &cb_consumer ;
-      pthread_t thread_video; 
-      rc_video = pthread_create(&thread_video, NULL, consume_thread , (void *)&videoData);
-      std::cout << "Thread over start waiting" << std::endl;
+      videoData.end = 16062 / 10;
+      videoData.cb = &cb_consumer;
+      videoData.rate = VIDEO_RATE; //27;
+      Name videoName = Name(prefix).append(filename).append("video").append("content");
 
-//      cb_consumer.player.consume_whole(videoConsumer, audioConsumer);
-//      time_t time_end_0  = std::time(0);
-//      std::cout << "After consume " << time_end_0 << std::endl;
-//      double seconds = difftime(time_end_0, time_start_0);
-//      std::cout << seconds << " seconds have passed" << std::endl;
 
-      sleep(3000); // because consume() is non-blocking
+      std::vector<Consumer *> audio_consumers;
+      std::vector<Consumer *> video_consumers;
+      std::deque<DataNode> video_q;
+      std::deque<DataNode> audio_q;
+
+      for (int j = 0; j < videoData.rate; j++) {
+        videoData.sampleConsumer = new Consumer(videoName, RDR);
+        consumer_setup(&videoData);
+        video_consumers.push_back(videoData.sampleConsumer);
+      }
+
+      for (int j = 0; j < audioData.rate; j++) {
+        audioData.sampleConsumer = new Consumer(audioName, RDR);
+        consumer_setup(&audioData);
+        audio_consumers.push_back(audioData.sampleConsumer);
+      } 
+
+//      consume_control(videoData.sampleConsumer, videoData.end);
+//      std::thread videoThread(consume_control, videoData.sampleConsumer, videoData.end);
+//      tp.schedule(boost::bind(consume_control, videoData.sampleConsumer, videoData.end));
+      int audio_counter = 0;
+      int video_counter = 0;
+      uint64_t audio_size = 0;
+      uint64_t video_size = 0;
+
+      pool tp_video(videoData.rate);
+      pool tp_audio(audioData.rate);
+
+      signal(SIGINT, sig_int);
+
+      while (audio_counter < audioData.end || video_counter < videoData.end) {
+
+        cb_consumer.buffers_a.clear();
+        cb_consumer.buffers_v.clear();
+
+        int init_video_counter = video_counter;
+        int init_audio_counter = audio_counter;
+
+        for (int j = 0; video_counter < videoData.end && j < videoData.rate; j++) {
+          cb_consumer.buffers_v[j] = NULL;
+          tp_video.schedule(boost::bind(consume_frame, "video", video_consumers[j], video_counter));
+          video_counter++;
+        }
+
+        for (int j = 0; audio_counter < audioData.end && j < audioData.rate; j++) {
+          cb_consumer.buffers_a[j] = NULL;
+          tp_audio.schedule(boost::bind(consume_frame, "audio", audio_consumers[j], audio_counter));
+          audio_counter++;
+        } 
+
+        tp_video.wait();
+        tp_audio.wait();
+        
+        for (int j = 0; init_video_counter < videoData.end && j < videoData.rate; j++) {
+          if (cb_consumer.buffers_v[j]) {
+            DataNode dataNode;
+            uint64_t bufferSize = cb_consumer.buffers_v[j]->size();
+            uint8_t* buffer = cb_consumer.buffers_v[j]->buf();
+            uint8_t* bufferTmp = new uint8_t[bufferSize];
+            memcpy (bufferTmp, buffer, bufferSize);
+            dataNode.length = bufferSize;
+            dataNode.data = (uint8_t *)bufferTmp;
+            video_q.push_back(dataNode);
+            video_size += dataNode.length;
+          }
+            
+          init_video_counter++;
+        }
+
+        for (int j = 0; init_audio_counter < audioData.end && j < audioData.rate; j++) {
+          if (cb_consumer.buffers_a[j]) {
+            DataNode dataNode;
+            uint64_t bufferSize = cb_consumer.buffers_a[j]->size();
+            uint8_t* buffer = cb_consumer.buffers_a[j]->buf();
+            uint8_t* bufferTmp = new uint8_t[bufferSize];
+            memcpy (bufferTmp, buffer, bufferSize);
+            dataNode.length = bufferSize;
+            dataNode.data = (uint8_t *)bufferTmp;
+            audio_q.push_back(dataNode);
+            audio_size += dataNode.length;
+          }
+
+          init_audio_counter++;
+        }
+
+      }
+
+      std::cout << "video_counter " << video_counter << std::endl;
+      std::cout << "video_q size " << video_q.size() <<  " buffer_size " << video_size << std::endl;
+      std::cout << "audio_counter " << audio_counter << std::endl;
+      std::cout << "audio_q size " << audio_q.size() <<  " buffer_size " << audio_size << std::endl;
+
+//      tp.wait();
+//      audioThread.join();
+//      videoThread.join();
+      sig_int(0);
+
+//      sleep(3000); // because consume() is non-blocking
       
     }
     catch(std::exception& e) {
--- src/test_speed_consumer.hpp
+++ src/test_speed_consumer.hpp
@@ -12,10 +12,20 @@
 #include <ctime>
 #include <boost/algorithm/string.hpp>
 #include <boost/thread/thread.hpp>
+#include "threadpool.hpp"
+
+using namespace boost::threadpool;
 
 namespace ndn{
 
 #define IDENTITY_NAME "/Lijing/Wang"
+
+  struct DataNode
+  {
+    uint64_t length;
+    uint8_t *data;
+  };
+
   class ConsumerCallback
   {
       
@@ -30,34 +40,73 @@ namespace ndn{
       interest_expr = 0;
       frame_cnt_v = 0;
       frame_cnt_a = 0;
+      frame_rate_v = 7;
+      frame_rate_a = 6;
+    }
+
+    ConsumerCallback(int rate_v, int rate_a) : 
+      frame_rate_v(rate_v), frame_rate_a(rate_a),
+      buffers_v(rate_v), buffers_a(rate_a)
+    {
+      data_ready = false;
+      payload_v = 0;
+      payload_a = 0;
+      interest_s = 0;
+      interest_r = 0;
+      interest_retx = 0;
+      interest_expr = 0;
+      frame_cnt_v = 0;
+      frame_cnt_a = 0;
     }
     
     void
     processTestPayload(Consumer& con, const uint8_t* buffer, size_t bufferSize) 
     {
+//      boost::lock_guard<boost::mutex> lock(frame_cnt_v_m);
+      frame_cnt_v_m.lock();
+
+      Name suffix;
+      con.getContextOption(SUFFIX, suffix);
+      int frameNumber = std::stoi(suffix.get(0).toUri());
+      printf("Video Frame Counter: %d suffix: %s frameNumber: %d\n", frame_cnt_v, suffix.toUri().c_str(), frameNumber);
       payload_v += bufferSize;
       frame_cnt_v++;
-      printf("Video Frame: %d\n", frame_cnt_v);
+
+      frame_cnt_v_m.unlock();
+
+      buffers_v[frameNumber % frame_rate_v] = make_shared<Buffer>(buffer, bufferSize);
     }
 
     void 
     processTestPayloadAudio(Consumer& con, const uint8_t* buffer, size_t bufferSize) 
     {
+//      boost::lock_guard<boost::mutex> lock(frame_cnt_a_m);
+      frame_cnt_a_m.lock();
+
+      Name suffix;
+      con.getContextOption(SUFFIX, suffix);
+      int frameNumber = std::stoi(suffix.get(0).toUri());
+      printf("Audio Frame Counter: %d suffix: %s frameNumber: %d\n", frame_cnt_a, suffix.toUri().c_str(), frameNumber);
       payload_a += bufferSize;
       frame_cnt_a++;
-      printf("Audio Frame: %d\n", frame_cnt_a);
+
+      frame_cnt_a_m.unlock();
+      buffers_a[frameNumber % frame_rate_a] = make_shared<Buffer>(buffer, bufferSize);
     }
 
     void
     processData(Consumer& con, const Data& data) {
+      boost::lock_guard<boost::mutex> lock(interest_r_m);
       interest_r++;
-  //    printf("DATA IN CNTX Name: %s  FinalBlockId: %s\n", data.getName().toUri().c_str(), data.getFinalBlockId().toUri().c_str());
+ //     printf("DATA IN CNTX Name: %s  FinalBlockId: %s\n", data.getName().toUri().c_str(), data.getFinalBlockId().toUri().c_str());
 
     }
     
     void
     processLeavingInterest(Consumer& con, Interest& interest) {
+      boost::lock_guard<boost::mutex> lock(interest_s_m);
       interest_s ++;
+//      printf("Leaving Interest Name: %s\n",interest.toUri().c_str());
   //    std::cout << "LEAVES " << interest.toUri() << std::endl;
     }
     
@@ -80,19 +129,29 @@ namespace ndn{
 
     void
     onRetx(Consumer& con, Interest& interest) {
+      boost::lock_guard<boost::mutex> lock(interest_retx_m);
       interest_retx ++;
       printf("Retransmitted %s\n", interest.getName().toUri().c_str());
     }
 
     void
     onExpr(Consumer& con, Interest& interest) {
+      boost::lock_guard<boost::mutex> lock(interest_expr_m);
       interest_expr ++;
       printf("Expired %s\n", interest.getName().toUri().c_str());
     }
     
     std::vector<std::string> list;
     boost::condition_variable cond;
+
     boost::mutex mut;
+    boost::mutex frame_cnt_v_m;
+    boost::mutex frame_cnt_a_m;
+    boost::mutex interest_s_m;
+    boost::mutex interest_r_m;
+    boost::mutex interest_retx_m;
+    boost::mutex interest_expr_m;
+
     bool data_ready;
     int frame_cnt_v;
     int frame_cnt_a;
@@ -102,179 +161,12 @@ namespace ndn{
     uint32_t interest_r;
     uint32_t interest_retx;
     uint32_t interest_expr;
+    uint32_t frame_rate_v;
+    uint32_t frame_rate_a;
+
+    std::vector<BufferPtr> buffers_v;
+    std::vector<BufferPtr> buffers_a;
   };
 
-
-  time_t time_start;
-  ConsumerCallback cb_consumer; 
-
-  static void sig_int(int num)
-  {
-    time_t time_end = std::time(0);
-    double seconds = difftime(time_end, time_start);
-    std::cout << "  " << std::endl;
-    std::cout << "｡:.ﾟヽ(*´∀`)ﾉﾟ. Test Result ヽ(✿ﾟ▽ﾟ)ノ  " << std::endl;
-    std::cout << "  " << std::endl;
-    std::cout << "-------- Throughput --------  " << std::endl;
-    std::cout << "Video Bytes: " << std::dec << cb_consumer.payload_v << "  PayLoad_Throughput: " << cb_consumer.payload_v/seconds << " Bytes/Seconds" <<std::endl;
-    std::cout << "Audio Bytes: " << cb_consumer.payload_a << "  PayLoad_Throughput: " << cb_consumer.payload_a/seconds << " Bytes/Seconds" <<std::endl;
-    std::cout << "Total Bytes: " << cb_consumer.payload_v + cb_consumer.payload_a << "  PayLoad_Throughput: " << (cb_consumer.payload_v + cb_consumer.payload_a)/seconds << " Bytes/Seconds" << std::endl;
-
-    std::cout << "Video Frames: " << std::dec << cb_consumer.frame_cnt_v << "  Frame_Throughput: " << cb_consumer.frame_cnt_v/seconds << " /Seconds" <<std::endl;
-    std::cout << "Audio Frames: " << std::dec << cb_consumer.frame_cnt_a << "  Frame_Throughput: " << cb_consumer.frame_cnt_a/seconds << " /Seconds" <<std::endl;
-    std::cout << "Total Frames: " << cb_consumer.frame_cnt_v + cb_consumer.frame_cnt_a << "  Frame_Throughput: " << (cb_consumer.frame_cnt_v + cb_consumer.frame_cnt_a)/seconds << " /Seconds" << std::endl;
-
-    std::cout << "  " << std::endl;
-    std::cout << "-------- Interest --------  " << std::endl;
-    std::cout << "Interest_Send: " << cb_consumer.interest_s << "  Interest_Send_Speed: " <<  cb_consumer.interest_s/seconds << " /Seconds" <<  std::endl;
-    std::cout << "Interest_Receive: " << cb_consumer.interest_r << "  Interest_Receive_Speed: " <<  cb_consumer.interest_r /seconds << " /Seconds" <<  std::endl;
-    std::cout << "Interest_Retransmit: " << cb_consumer.interest_retx << " Interest_Retransfer_Speed: " << cb_consumer.interest_retx/seconds << " /Seconds" << std::endl;
-    std::cout << "Interest_Expire: " << cb_consumer.interest_expr << " Interest_Retransfer_Speed: " << cb_consumer.interest_expr/seconds << " /Seconds" << std::endl;
-    std::cout << "Interest_Useful Percentage: " << double(cb_consumer.interest_r)/double(cb_consumer.interest_s)*100 <<"%" << std::endl;
-    std::cout << "  " << std::endl;
-    std::cout << "---------- OVER ----------  " << seconds <<" seconds" << std::endl;
-
-    return;
-  }
-
-
-  class Verificator
-  {
-  public:
-    Verificator()
-    {
-      Name identity(IDENTITY_NAME);
-      Name keyName = m_keyChain.getDefaultKeyNameForIdentity(identity);
-      m_publicKey = m_keyChain.getPublicKey(keyName); 
-    };
-    
-    bool
-    onPacket(Data& data)
-    {
-    //  return true;
-      if (Validator::verifySignature(data, *m_publicKey))
-      {
-        std::cout << "Verified Name:" << data.getName() <<  std::endl;
-        return true;
-      }
-      else
-      {
-        std::cout << "NOT Verified" << data.getName() << std::endl;
-        return false;
-      }
-    }
-    
-  private:
-    KeyChain m_keyChain;
-    shared_ptr<PublicKey> m_publicKey;
-  };
-
-  struct Consumer_Need
-  {
-    Name prefix;
-    std::string name;
-    ConsumerCallback *cb;
-  };
-
-  static void
-  *consume_thread (void * threadData)
-  {
-    Verificator* verificator = new Verificator();
-    Consumer_Need *con;
-    con = (Consumer_Need *) threadData;
-    int end;
-    int sleeptime;
-
-    Name sampleName = con->prefix;
-    sampleName.append(con->name + "/" + "content");
-
-    Consumer* sampleConsumer = new Consumer(sampleName, RDR);
-
-    if(con->name == "video")
-    {
-//      sampleConsumer->setContextOption(EMBEDDED_MANIFESTS, true);
-//      sampleConsumer->setContextOption(CONTENT_RETRIEVAL_SIZE, 1024*1024);
-//      sampleConsumer->setContextOption(MIN_WINDOW_SIZE, 8);
-      sampleConsumer->setContextOption(CONTENT_RETRIEVED, 
-                          (ConsumerContentCallback)bind(&ConsumerCallback::processTestPayload, con->cb, _1, _2, _3));
-
-      ndn::shared_ptr<Face> f1;
-      sampleConsumer->getContextOption(FACE, f1);
-//      std::cout << " Video Face = " << f1 << std::endl;
-
-//      end = con->cb->finalframe_video;
-      end = 16062;
-      sleeptime = 10;
-    }else
-    {
-      sampleConsumer->setContextOption(CONTENT_RETRIEVED, 
-                          (ConsumerContentCallback)bind(&ConsumerCallback::processTestPayloadAudio, con->cb, _1, _2, _3));
-      ndn::shared_ptr<Face> f2;
-      sampleConsumer->getContextOption(FACE, f2);
-//      std::cout << " Audio Face = " << f2 << std::endl;
-      sleeptime = 10;
-//      end = con->cb->finalframe_audio;
-      end = 13838;
-    }
-        
-    sampleConsumer->setContextOption(MUST_BE_FRESH_S, true);
-    sampleConsumer->setContextOption(INTEREST_LIFETIME, 500);
-
-    sampleConsumer->setContextOption(INTEREST_RETX,16); //Retransmitted Attempted Time.
-   // there is no need for other callback now
-//    sampleConsumer->setContextOption(DATA_TO_VERIFY,
-//                    (DataVerificationCallback)bind(&Verificator::onPacket, verificator, _1));
-//    sampleConsumer->setContextOption(MIN_WINDOW_SIZE, 1);
-//    sampleConsumer->setContextOption(MAX_WINDOW_SIZE, 10);
-    sampleConsumer->setContextOption(INTEREST_LEAVE_CNTX, 
-                              (ConsumerInterestCallback)bind(&ConsumerCallback::processLeavingInterest, con->cb, _1, _2));
-    sampleConsumer->setContextOption(INTEREST_RETRANSMIT, 
-                              (ConsumerInterestCallback)bind(&ConsumerCallback::onRetx, con->cb, _1, _2));
-    sampleConsumer->setContextOption(INTEREST_EXPIRED, 
-                              (ConsumerInterestCallback)bind(&ConsumerCallback::onExpr, con->cb, _1, _2));
-    sampleConsumer->setContextOption(DATA_ENTER_CNTX, 
-                              (ConsumerDataCallback)bind(&ConsumerCallback::processData, con->cb, _1, _2));
-
-    
-    std::cout << "HERE!" << std::endl;
-    for (int i=0; i<end; i++)
-    { 
-      Name sampleSuffix(std::to_string(i));
-//      usleep(sleeptime); 
-      sampleConsumer->setContextOption(MIN_WINDOW_SIZE, 10);
-      sampleConsumer->setContextOption(MAX_WINDOW_SIZE, 20);
-      sampleConsumer->consume(sampleSuffix);
-//      if (con->name == "video") {
-//        std::cout << "video frame number " << std::dec << i << std::endl;
-//        con->cb->cur_frame_v = i;
-//        boost::unique_lock<boost::mutex> lock(con->cb->mut_payload_v);
-//        while(!con->cb->data_ready_payload_v)
-//        {
-//          std::cout << "I'm HERE! Video Start waiting" << std::endl;
-//          con->cb->cond_payload_v.wait(lock);
-//          std::cout << "I'm HERE! Video Over waiting" << std::endl;
-//        }
-//        con->cb->data_ready_payload_v = false;
-//      } else {
-//        std::cout << "audio frame number " << std::dec << i  << std::endl;
-//        con->cb->cur_frame_a = i;
-//        boost::unique_lock<boost::mutex> lock(con->cb->mut_payload_a);
-//        while(!con->cb->data_ready_payload_a)
-//        {
-//          std::cout << "I'm HERE! Audio Start waiting" << std::endl;
-//          con->cb->cond_payload_a.wait(lock);
-//          std::cout << "I'm HERE! Audio Over waiting" << std::endl;
-//        }
-//        con->cb->data_ready_payload_a = false;
-//      }
-//      if(i % 500 == 0)
-//      {
-//        std::cout << "Time Reached!" << i << std::endl;
-//        sleep(sleeptime);
-//      }
-    }
-    sig_int(0);
-    pthread_exit(NULL);
-  }
 }
 #endif
